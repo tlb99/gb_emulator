87772faed3f584afbab5ef4e2f3478115f46a8ab
tlbot <28433525+tlb99@users.noreply.github.com>
2026-01-26 18:28:31 -0800

    create gb memory class

---
+++ include/GBMemory.h
@@ -0,0 +1,16 @@
+﻿//
+// Created by Tony on 1/26/2026.
+//
+
+#ifndef GB_EMULATOR_GBMEMORY_H
+#define GB_EMULATOR_GBMEMORY_H
+#include <cstddef>
+
+class GBMemory {
+public:
+
+private:
+    static std::byte memory_;
+};
+
+#endif //GB_EMULATOR_GBMEMORY_H
