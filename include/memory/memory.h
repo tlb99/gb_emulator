2f3e8cf1c3727f2a9939822fba11ba223fbcb250
tlbot <28433525+tlb99@users.noreply.github.com>
2026-01-31 18:47:52 -0800

    create memory and ppu classes

---
+++ include/memory/memory.h
@@ -0,0 +1,13 @@
+﻿//
+// Created by Tony on 1/30/2026.
+//
+
+#ifndef GB_EMULATOR_MEMORY_H
+#define GB_EMULATOR_MEMORY_H
+
+namespace GameBoy {
+    class memory {
+    };
+} // GameBoy
+
+#endif //GB_EMULATOR_MEMORY_H
---
+++ src/memory/memory.cc
@@ -0,0 +1,8 @@
+﻿//
+// Created by Tony on 1/30/2026.
+//
+
+#include "memory/memory.h"
+
+namespace GameBoy {
+} // GameBoy
