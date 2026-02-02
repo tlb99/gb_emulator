b6fd6ac94a989573e6af838796ee3b74985cacad
tlbot <28433525+tlb99@users.noreply.github.com>
2026-02-01 16:20:40 -0800

    fleshed out cpu header

--- include/processor/cpu.h
+++ include/processor/cpu.h
@@ -4,19 +4,50 @@
 
 #ifndef GB_EMULATOR_CPU_H
 #define GB_EMULATOR_CPU_H
+#include <bits/shared_ptr.h>
 
-namespace GameBoy {
+#include <cstdint>
+
+#include "memory/memory.h"
 
 class CPU {
-  // TODO structure this class to match the Game Boy's CPU
 public:
+  explicit CPU(Memory& memory)
+  : memory_(memory),
+    a_(0), f_(0), b_(0), c_(0), d_(0), e_(0), h_(0), l_(0),
+    pc_(0), sp_(0), zero_(false), substraction_(false), half_carry_(false), carry_(false) {}
+  void Cycle();
+
+private:
   void Fetch();
   void Decode();
   void Execute();
-private:
+
+  Memory& memory_;
+
+  // 8-bit Registers
+  uint8_t a_;
+  uint8_t f_;
+
+  uint8_t b_;
+  uint8_t c_;
+
+  uint8_t d_;
+  uint8_t e_;
+
+  uint8_t h_;
+  uint8_t l_;
+
+  // Program counter and stack pointer
+  uint16_t pc_;  // Program counter
+  uint16_t sp_;  // Stack pointer
+
+  // Flags
+  bool zero_;
+  bool substraction_;
+  bool half_carry_;
+  bool carry_;
 
 };
-
-}  // namespace GameBoy
 
 #endif  // GB_EMULATOR_CPU_H
