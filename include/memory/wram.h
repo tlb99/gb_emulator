abff028e93d631e3b01e995069bf4f2f03ac2d4d
tlbot <28433525+tlb99@users.noreply.github.com>
2026-02-02 20:54:43 -0800

    minor comment move

--- include/memory/wram.h
+++ include/memory/wram.h
@@ -11,8 +11,7 @@ class WRAM {
 public:
   WRAM() : wram_(8192) {}
 private:
-  // According to Pan Docs, WRAM is 8 KiB by default on most Game Boy models but the Game Boy Color, which has 32 KiB
-  std::vector<uint8_t> wram_;
+  std::vector<uint8_t> wram_; // According to Pan Docs, WRAM is 8 KiB by default on most Game Boy models but the Game Boy Color, which has 32 KiB
 };
 
 #endif //GB_EMULATOR_MEMORY_H
