85e896248b6df3aa779e565de67e9969d53e8e85
tlbot <28433525+tlb99@users.noreply.github.com>
2026-02-04 19:25:29 -0800

    added wram constexpr variables for clarity on wram start and end ranges as well as size. also implemented read and getwram functions

--- include/memory/wram.h
+++ include/memory/wram.h
@@ -9,11 +9,18 @@
 
 class WRAM {
 public:
-  WRAM() : wram_(8192) {}
+  static constexpr uint16_t WRAM_START = 0xC000;
+  static constexpr uint16_t WRAM_END = 0xDFFF;
+  static constexpr size_t WRAM_SIZE = 0x2000;  // According to Pan Docs, WRAM is 8 KiB by default on most Game Boy models but the Game Boy Color, which has 32 KiB
 
-  void Write(uint8_t value, uint16_t address);
+  WRAM() : wram_(WRAM_SIZE) {}
+
+  void Write(const uint8_t &value, const uint16_t &address);
+  [[nodiscard]] uint8_t Read(const uint16_t &address) const;
+
+  [[nodiscard]] const std::vector<uint8_t>& GetWRAM() const { return wram_; }
 private:
-  std::vector<uint8_t> wram_; // According to Pan Docs, WRAM is 8 KiB by default on most Game Boy models but the Game Boy Color, which has 32 KiB
+  std::vector<uint8_t> wram_;
 };
 
 #endif //GB_EMULATOR_MEMORY_H
--- src/memory/wram.cc
+++ src/memory/wram.cc
@@ -2,10 +2,22 @@
 // Created by Tony on 1/30/2026.
 //
 
+#include <spdlog/spdlog.h>
 #include "memory/wram.h"
 
-void WRAM::Write(const uint8_t value, const uint16_t address) {
-  if (address >= 0xC000 && address < 0xE000) {
-    wram_[address - 0xC000] = value;
+void WRAM::Write(const uint8_t& value, const uint16_t& address) {
+  if (address >= WRAM_START && address <= WRAM_END) {
+    wram_[address - WRAM_START] = value;
+  } else {
+    spdlog::warn("Invalid WRAM write: addr=0x{:04X}, value=0x{:02X}", address, value);
   }
+}
+
+uint8_t WRAM::Read(const uint16_t& address) const {
+  if (address >= WRAM_START && address <= WRAM_END) {
+    return wram_[address - WRAM_START];
+  }
+
+  spdlog::warn("Invalid WRAM read: addr=0x{:04X}", address);
+  return 0xFF;
 }
