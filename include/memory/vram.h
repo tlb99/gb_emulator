13a11cfe0b1a8b071d988c384762a5d0bd6307f8
tlbot <28433525+tlb99@users.noreply.github.com>
2026-02-22 11:47:48 -0800

    create vram class

---
+++ include/memory/vram.h
@@ -0,0 +1,23 @@
+﻿//
+// Created by Tony on 2/22/2026.
+//
+
+#ifndef GB_EMULATOR_VRAM_H
+#define GB_EMULATOR_VRAM_H
+#include "ram.h"
+
+
+class VRAM : public RAM {
+public:
+  static constexpr uint16_t START = 0x8000;
+  static constexpr uint16_t END = 0x9FFF;
+  static constexpr uint16_t SIZE = 0x2000;
+
+  VRAM() : RAM(START, END, SIZE) {}
+
+protected:
+  [[nodiscard]] std::string get_class_name() const override { return "VRAM";}
+};
+
+
+#endif //GB_EMULATOR_VRAM_H
---
+++ src/memory/vram.cc
@@ -0,0 +1,5 @@
+﻿//
+// Created by Tony on 2/22/2026.
+//
+
+#include "memory/vram.h"
