964bc1fc623d1d98a9c0341c86f73a71459a4738
tlbot <28433525+tlb99@users.noreply.github.com>
2026-07-26 18:31:39 -0700

    create initial tile class

---
+++ include/graphics/tile.h
@@ -0,0 +1,25 @@
+﻿//
+// Created by Tony on 2/23/2026.
+//
+
+#ifndef GB_EMULATOR_TILE_H
+#define GB_EMULATOR_TILE_H
+#include <cstdint>
+
+
+class Tile {
+public:
+
+private:
+  enum class Palette {
+    WHITE,
+    LIGHT_GRAY,
+    DARK_GRAY,
+    BLACK
+  };
+
+  Palette data_[8][8];
+};
+
+
+#endif //GB_EMULATOR_TILE_H
---
+++ src/graphics/tile.cc
@@ -0,0 +1,5 @@
+﻿//
+// Created by Tony on 2/23/2026.
+//
+
+#include "graphics/tile.h"
