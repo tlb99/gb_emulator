a66e012f0639648801c4626400fc9a79beea8161
tlbot <28433525+tlb99@users.noreply.github.com>
2026-07-26 18:32:04 -0700

    create initial sdl2renderer class

---
+++ include/graphics/sdl2/sdl_2_renderer.h
@@ -0,0 +1,27 @@
+﻿//
+// Created by Tony on 2/23/2026.
+//
+
+#ifndef GB_EMULATOR_SDL_2_RENDERER_H
+#define GB_EMULATOR_SDL_2_RENDERER_H
+
+#include <SDL2/SDL.h>
+
+class SDL2Renderer {
+public:
+  // Hardcoded values for now
+  const int kDefaultWidth = 160 * 15;
+  const int kDefaultHeight = 144 * 15;
+
+  SDL2Renderer() : window_(nullptr), surface_(nullptr) {}
+
+  bool Init();
+private:
+
+  SDL_Window* window_;
+
+  SDL_Surface* surface_;
+};
+
+
+#endif //GB_EMULATOR_SDL_2_RENDERER_H
---
+++ src/graphics/sdl2/sdl_2_renderer.cc
@@ -0,0 +1,35 @@
+﻿//
+// Created by Tony on 2/23/2026.
+//
+
+#include "../../../include/graphics/sdl2/sdl_2_renderer.h"
+
+#include <spdlog/spdlog.h>
+
+bool SDL2Renderer::Init() {
+  //Initialization flag
+  bool success = true;
+
+  //Initialize SDL
+  if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+  {
+    spdlog::error( "SDL could not initialize! SDL_Error: {}", SDL_GetError() );
+    success = false;
+  }
+  else
+  {
+    //Create a window
+    window_ = SDL_CreateWindow( "GB Emulator", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, kDefaultWidth, kDefaultHeight, SDL_WINDOW_SHOWN );
+    if( window_ == nullptr )
+    {
+      spdlog::error( "Window could not be created! SDL_Error: {}", SDL_GetError() );
+      success = false;
+    }
+    else
+    {
+      surface_ = SDL_GetWindowSurface( window_ );
+    }
+  }
+
+  return success;
+}
